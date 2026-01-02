2e5797fb68893622d71ee57989cec2bd4bf16cef
khaled cherif <khaledcherif89@gmail.com>
2026-01-02 20:52:24 +0200

    Implement dequeue, getFront, isFound, and clearAllItems methods in QueueLinkedList; update main program to test new functionalities

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -21,6 +21,31 @@ int main()
     myQueue.display();
     }
 
+    //test dequeue 
+    myQueue.dequeue();
+    cout << "--- Current Queue Content after one dequeue ---" << endl;
+    myQueue.display();
+
+    //test getFront
+    cout << "Front item is: " << myQueue.getFront() << endl;
+
+    //test isFound 
+    int item;
+    cout<<"enter item to search for in the Queue"<<endl;
+    cin>>item;
+    if(myQueue.isFound(item)){
+        cout <<"item found in the Queue"<<endl;
+        }
+        else{
+            cout <<"item not found in the Queue"<<endl;}
+    
+    //test clearALlItems
+    cout<<"Clearing all items from the Queue..."<<endl;
+    myQueue.clearAllItems();
+    if(myQueue.isEmpty())
+       cout<<"The Queue is empty"<<endl;
+    else
+       cout << "The Queue is not empty"<<endl;
     
 
 
